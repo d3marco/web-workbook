d889300d9c36e7832af5991bc71f670728f48591
DeMarco <demarcospears22@gmail.com>
2018-08-27 18:43:43 -0500

    updated dom-practice

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,5 +1,19 @@
 'use strict';
 
 document.addEventListener("DOMContentLoaded", function(event) {
-window.alert("there are 5 list items!");
-});
+
+window.onload = function() {
+
+var count = document.querySelectorAll('li');
+console.log(count.length);
+
+var newh2 = document.createElement('h2');
+newh2.innerHTML = 'You have ' + count.length + ' items in your shopping cart.'
+document.getElementsByTagName('h1')[0].append(newh2);
+alert('You have ' + count.length + ' items in your shopping cart.');
+
+
+function addedItem(){
+  var ul
+}
+}
