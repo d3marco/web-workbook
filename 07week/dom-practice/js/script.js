7d77007822ca4c83868b968f8eec2dc78f95a73f
DeMarco <demarcospears22@gmail.com>
2018-09-05 19:40:04 -0500

    dom-practice

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -3,17 +3,6 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
 window.onload = function() {
-
-var count = document.querySelectorAll('li');
-console.log(count.length);
-
-var newh2 = document.createElement('h2');
-newh2.innerHTML = 'You have ' + count.length + ' items in your shopping cart.'
-document.getElementsByTagName('h1')[0].append(newh2);
-alert('You have ' + count.length + ' items in your shopping cart.');
-
-
-function addedItem(){
-  var ul
+  alert('There are 4 list items');
 }
 }
