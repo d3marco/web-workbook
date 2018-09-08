5cb5a11b1297e84b9fd93f7a99f77840a335f1b1
DeMarco <demarcospears22@gmail.com>
2018-09-07 23:32:21 -0500

    tower-of-haoni

--- 09week/towers-of-hanoi/script.js
+++ 09week/towers-of-hanoi/script.js
@@ -1,5 +1,28 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic here
+  console.log('this is working')
+  $(function() {
+    console.log('in the drag function')
+    $('[data-block]').draggable({
+      revert: 'invalid'
+    });
+
+    $('[data-stack]').droppable({
+      drop: function(event, ui) {
+        console.log('in the drop function')
+        let $last = ($(this).children().last()).data('block');
+
+        $(ui.draggable).appendTo(this).attr('style', 'position:"relative"');
+
+        let $dragging = $(ui.draggable).data('block');
+
+        if ($dragging > $last) {
+          $(ui.draggable).draggable('option', 'revert', true);
+        } else {
+          $(ui.draggable).appendTo(this).attr('style', 'position:"relative"');
+        }
+      }
+    });
+  });
 });
